a72efdc8a9b403e0e4cf6d11af2d8156b1c18dad
Glen Knight <glenak1911@gmail.com>
2016-06-01 17:59:35 -0400

    Added paddle drawing logic

--- script.js
+++ script.js
@@ -6,11 +6,21 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 5;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
 
+/*Main function that calls the draw method every 10ms */
 function initialize(){
+	document.addEventListener("keydown", keyDownHandler, false);
+	document.addEventListener("keyup", keyUpHandler, false);
+	
 	setInterval(draw, 10);
 }
 
+/*Draws the actual ball and fills it with a blue color*/
 function drawBall(){
 	ctx.beginPath();
 	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -18,10 +28,11 @@ function drawBall(){
 	ctx.fill();
 	ctx.closePath();	
 }
-	
+/*This function clears the canvas, and draws at the specified x and y location,  and determines collision with the wall*/
 function draw(){	
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
+	drawPaddle();
 	
 	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius){
 		dx = -dx;
@@ -34,4 +45,33 @@ function draw(){
 	x+=dx;
 	y+=dy;
 }
+
+function drawPaddle(){
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+	ctx.fillStyle="#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
+function keyDownHandler(e){
+	if(e.keyCode == 39){
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37){
+		leftPressed = false;
+	}
+}
+
+function keyUpHandler(e){
+	if(e.keyCode == 39){
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37){
+		leftPressed = false;
+	}
+	
+	
+}
+
 
